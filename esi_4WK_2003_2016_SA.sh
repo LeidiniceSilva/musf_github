75b9cca71245e041e6077724cc3508d2d7f1dd11
Leidinice Silva <leidinicesilva@gmail.com>
2018-05-15 14:37:48 -0300

    deleted files

--- esi_4WK_2003_2016_SA.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# gerar arquivo .nc apartir do .ctl 
-# Leidinice Silva / FUNCEME
-
-lats4d.sh -q -format netcdf -lat -60 15 -lon -90 -30 -i DFPPM_4WK_SMN_2007.ctl -o esi_4WK_SMN_2007_SA
-
-
-
-
